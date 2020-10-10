201b1767f696cec73c4033dcf32c8f76e2b3d786
koro0ne <lem17902@spengergasse.at>
2020-10-10 12:55:09 +0200

    changed z after probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1052,7 +1052,7 @@
 #define Z_CLEARANCE_DEPLOY_PROBE   10 // Z Clearance for Deploy/Stow
 #define Z_CLEARANCE_BETWEEN_PROBES  4 // Z Clearance between probe points
 #define Z_CLEARANCE_MULTI_PROBE     4 // Z Clearance between multiple probes
-#define Z_AFTER_PROBING           200 // Z position after probing is done
+#define Z_AFTER_PROBING           50 // Z position after probing is done
 
 #define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
 
